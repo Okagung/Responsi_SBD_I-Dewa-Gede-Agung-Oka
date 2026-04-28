e016d177f4cf65a0ded2250bcc5acf830f51d11e
Okagung <idewagede79@gmail.com>
2026-04-28 18:26:30 +0700

    isriwil

---
+++ src/controllers/loanController.js
@@ -0,0 +1,37 @@
+import { LoanModel } from '../models/loanModel.js';
+
+export const LoanController = {
+  async createLoan(req, res) {
+    const { book_id, member_id, due_date } = req.body;
+    try {
+      const loan = await LoanModel.createLoan(book_id, member_id, due_date);
+      res.status(201).json({
+        message: "Peminjaman berhasil dicatat!",
+        data: loan
+      });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  },
+
+  async getLoans(req, res) {
+    try {
+      const loans = await LoanModel.getAllLoans();
+      res.json(loans);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  async getTopBorrowers(req, res) {
+    try {
+      const topBorrowers = await LoanModel.getTopBorrowers();
+      res.status(200).json({
+        message: "Berhasil mengambil Top 3 Peminjam",
+        data: topBorrowers
+      });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  }
+};
